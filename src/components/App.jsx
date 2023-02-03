e033bb5c09c3587ed5929cec7eba4f8b3a13d3d5
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-02-03 13:56:03 +0200

    colors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
+        color: '#FFBF00',
       }}
     >
       Pets Support
