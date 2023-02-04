4c4f2904a2af7aa242ee32938d20c164c44da80c
Anatolii <webnoveltystudio@gmail.com>
2023-02-04 19:43:24 +0100

    tek

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,30 @@
 import useMatchMedia from '../../hooks/use-match-media';
+import { BsPersonCircle } from 'react-icons/bs';
 
-import { Nav, NavLinkMenu} from './Navigation.styled';
+import { Nav, NavLinkMenu, NavMobile, Button } from './Navigation.styled';
 const Navigation = () => {
   const { isMobile } = useMatchMedia();
 
   return (
     <>
       {isMobile ? (
-        <p>Мобилка</p>
-      ) : (
-        <Nav>  
+        <>
+          <Button>
+            <NavLinkMenu to="/user">
+              <BsPersonCircle size={24} color={'#fff'} /> 
+            </NavLinkMenu>
+          </Button>
+          <NavMobile>
             <NavLinkMenu to="/news">News</NavLinkMenu>
             <NavLinkMenu to="/notices">Find pet</NavLinkMenu>
-            <NavLinkMenu to="/friends">Our friends</NavLinkMenu>     
+            <NavLinkMenu to="/friends">Our friends</NavLinkMenu>
+          </NavMobile>
+        </>
+      ) : (
+        <Nav>
+          <NavLinkMenu to="/news">News</NavLinkMenu>
+          <NavLinkMenu to="/notices">Find pet</NavLinkMenu>
+          <NavLinkMenu to="/friends">Our friends</NavLinkMenu>
         </Nav>
       )}
     </>
