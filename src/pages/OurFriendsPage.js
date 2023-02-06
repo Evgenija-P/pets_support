6476726ec620a025ec6b057326dcf71c61123911
Evgenija-P <e.a.poduzova@gmail.com>
2023-02-06 23:31:25 +0200

    modal-bug

--- src/pages/OurFriendsPage.js
+++ src/pages/OurFriendsPage.js
@@ -1,6 +1,7 @@
 import { Helmet } from 'react-helmet';
 import React, { useState } from 'react';
 import Modal from '../components/Modal';
+// import datas from '../data/sponsors.json';
 
 const OurFriendsPage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -9,12 +10,19 @@ const OurFriendsPage = () => {
     setShowModal(!showModal);
   }
 
+  // console.log(datas);
+
   return (
     <div>
       <Helmet>
         <title>OurFriendsPage</title>
       </Helmet>
       <button onClick={toggleModal}>Click me!!!</button>
+      {/* <div>
+        {datas.map(({ title, address, email }) => (
+          <li>{title}</li>
+        ))}
+      </div> */}
 
       {showModal && (
         <Modal onClose={toggleModal} title={'add pets'} type={'notice'}>
