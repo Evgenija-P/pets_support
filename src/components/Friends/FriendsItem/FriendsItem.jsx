22d4f4110d4dbf208dab7c6243da356e2a66d453
Evgenija-P <e.a.poduzova@gmail.com>
2023-02-08 01:45:08 +0200

    datas

--- src/components/Friends/FriendsItem/FriendsItem.jsx
+++ src/components/Friends/FriendsItem/FriendsItem.jsx
@@ -1,4 +1,5 @@
 import FriendsAbout from '../FriendsAbout/FriendsAbout';
+import { nanoid } from 'nanoid';
 import {
   FriendsItemWrapper,
   FriendsWrapper,
@@ -7,6 +8,8 @@ import {
   FriendsItemTitle,
   FriendsURL,
 } from './FriendsItem.styles';
+
+import datas from '../../../data/sponsors.json';
 
 const data = {
   title: "Притулок для бездомних тварин 'Сіріус'",
@@ -28,24 +31,47 @@ const data = {
   email: null,
 };
 
+console.log(datas);
 const FriendsItem = () => {
+  const items = datas;
+
   return (
-    <FriendsItemWrapper>
-      <FriendsURL href={`${data.url}`} target="_blank">
-        <FriendsItemTitle>{data.title}</FriendsItemTitle>
-      </FriendsURL>
-      <FriendsWrapper>
-        <FriendsImgWrapper>
-          <FriendsImg src={`${data.imageUrl}`} alt={data.title} />
-        </FriendsImgWrapper>
-        <FriendsAbout
-          adress={data.address}
-          workDays={data.workDays}
-          phone={data.phone}
-          email={data.email}
-        />
-      </FriendsWrapper>
-    </FriendsItemWrapper>
+    <>
+      {items.map(item => (
+        <FriendsItemWrapper key={nanoid()}>
+          <FriendsURL href={`${item.url}`} target="_blank">
+            <FriendsItemTitle>{item.title}</FriendsItemTitle>
+          </FriendsURL>
+          <FriendsWrapper>
+            <FriendsImgWrapper>
+              <FriendsImg src={`${item.imageUrl}`} alt={item.title} />
+            </FriendsImgWrapper>
+            <FriendsAbout
+              adress={item.address}
+              workDays={item.workDays}
+              phone={item.phone}
+              email={item.email}
+            />
+          </FriendsWrapper>
+        </FriendsItemWrapper>
+      ))}
+    </>
+    // <FriendsItemWrapper>
+    //   <FriendsURL href={`${data.url}`} target="_blank">
+    //     <FriendsItemTitle>{data.title}</FriendsItemTitle>
+    //   </FriendsURL>
+    //   <FriendsWrapper>
+    //     <FriendsImgWrapper>
+    //       <FriendsImg src={`${data.imageUrl}`} alt={data.title} />
+    //     </FriendsImgWrapper>
+    //     <FriendsAbout
+    //       adress={data.address}
+    //       workDays={data.workDays}
+    //       phone={data.phone}
+    //       email={data.email}
+    //     />
+    //   </FriendsWrapper>
+    // </FriendsItemWrapper>
   );
 };
 
