35aafa689b95d411364b649a0e66fbb89a009d3c
VitaPetrushyna <99089205+VitaPetrushyna@users.noreply.github.com>
2023-02-07 19:46:56 +0200

    adds icon in search

--- src/components/NoticesElements/NoticesSearch/NoticesSearch.styled.js
+++ src/components/NoticesElements/NoticesSearch/NoticesSearch.styled.js
@@ -2,6 +2,11 @@ import styled from '@emotion/styled';
 import { media, colors, fontWeights } from '../../../styles/stylesLayout';
 
 export const SearchForm = styled.form`
+  width: 100%;
+  height: 100%;
+
+  position: relative;
+
   display: flex;
   justify-content: center;
 `;
@@ -9,6 +14,8 @@ export const SearchForm = styled.form`
 export const SearchInput = styled.input`
   width: 100%;
   height: 40px;
+
+  display: inline-block;
 
   padding: 8px 12px;
   border-radius: 40px;
@@ -44,5 +51,27 @@ export const SearchInput = styled.input`
     ${media.tabletAndDesktop} {
       font-size: 20px;
     }
+  }
+`;
+
+export const SearchFormButton = styled.button`
+  position: absolute;
+  left: 70%;
+  top: 10%;
+
+  display: block;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  background-size: 100%;
+  /* background-repeat: no-repeat;
+  background-position: center; */
+  opacity: 0.6;
+  /* transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1); */
+  cursor: pointer;
+  outline: none;
+  background-color: ${colors.white};
+  :hover {
+    opacity: 1;
   }
 `;
--- src/pages/NoticesPage.js
+++ src/pages/NoticesPage.js
@@ -1,30 +1,32 @@
 import { Helmet } from 'react-helmet';
 
-// import { useEffect, useState } from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import getNoticesByCategory from '../servises/fetchNotices';
 
 import SectionContainer from '../components/SectionContainer';
 import NoticesCategoriesNav from '../components/NoticesElements/NoticesCategoriesNav/NoticesCategoriesNav';
 import NoticesSearch from '../components/NoticesElements/NoticesSearch/NoticesSearch';
-// import AddNoticeButton from '../components/NoticesElements/AddNoticeButton/AddNoticeButton';
-// import NoticesCategoriesList from '../components/NoticesElements/NoticesCategoriesList/NoticesCategoriesList';
+import AddNoticeButton from '../components/NoticesElements/AddNoticeButton/AddNoticeButton';
+import NoticesCategoriesList from '../components/NoticesElements/NoticesCategoriesList/NoticesCategoriesList';
 
 const NoticesPage = () => {
-  // const [notices, setNotices] = useState([]);
-  // const { pathname: category } = useLocation();
+  const [notices, setNotices] = useState([]);
+  const { pathname: category } = useLocation();
 
-  // useEffect(() => {
-  //   console.log(category);
+  useEffect(() => {
+    console.log(category);
 
-  //   const fetchNotices = async () => {
-  //     const { message: result } = await getNoticesByCategory(category);
-  //     setNotices(result);
-  //     try {
-  //     } catch (error) {}
-  //   };
+    const fetchNotices = async () => {
+      const { message: result } = await getNoticesByCategory(category);
+      setNotices(result);
+      try {
+      } catch (error) {}
+    };
 
-  //   fetchNotices();
-  // }, [category]);
+    fetchNotices();
+  }, [category]);
 
   return (
     <>
