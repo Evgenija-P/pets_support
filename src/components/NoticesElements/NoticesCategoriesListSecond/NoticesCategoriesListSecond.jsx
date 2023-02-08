f938f9f2d892f2eb3ee4b0d548fe2028b2ab580f
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-08 03:22:23 +0200

    favorite list add

--- src/components/NoticesElements/NoticesCategoriesListSecond/NoticesCategoriesListSecond.jsx
+++ src/components/NoticesElements/NoticesCategoriesListSecond/NoticesCategoriesListSecond.jsx
@@ -13,16 +13,47 @@ import {
 
 import notFoundNoticesImage from '../../../img/notFoundNoticesImage.jpg';
 
-import { useSelector, useDispatch } from 'react-redux';
-
+// import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import useAuth from '../../../hooks/useAuth.js';
 import {
   selectNotices,
-  // selectFilter,
-  // selectVisibleContacts,
+  selectFavoriteNotices,
 } from '../../../redux/notices/selectors';
 
+import { selectUser } from '../../../redux/auth/selectors.js';
+
 const NoticesCategoriesListSecond = () => {
-  const notices = useSelector(selectNotices);
+  const noticesRaw = useSelector(selectNotices);
+  const favorite = useSelector(selectFavoriteNotices);
+  const user = useSelector(selectUser);
+  const userFavoriteNotices = () => {
+    const noticesWithFavorite = noticesRaw.map(notice => {
+      if (favorite.find(fav => fav._id === notice._id)) {
+        console.log('favorite', notice._id);
+        return { ...notice, favorite: true };
+      }
+      return { ...notice, favorite: false };
+    });
+    // console.log('favoriteList', noticesWithFavorite);
+    return noticesWithFavorite;
+  };
+  const noticesWithFavorite = userFavoriteNotices();
+
+  const isOwnerNotices = () => {
+    const noticesOwn = noticesWithFavorite.map(notice => {
+      console.log('user._id', user);
+      if (notice.owner === user._id) {
+        console.log('owner', notice.owner);
+        return { ...notice, isOwner: true };
+      }
+      return { ...notice, isOwner: false };
+    });
+    console.log('ownerList', noticesOwn);
+    return noticesOwn;
+  };
+  const notices = isOwnerNotices();
+  const isLogined = useAuth();
   return (
     <NoticesList>
       {notices.map(
@@ -35,6 +66,8 @@ const NoticesCategoriesListSecond = () => {
           location,
           age,
           price,
+          favorite,
+          isOwner,
         }) => (
           <NoticesItem key={_id}>
             <NoticesTop>
@@ -53,6 +86,8 @@ const NoticesCategoriesListSecond = () => {
                 <NoticesTag>Place: {location}</NoticesTag>
                 <NoticesTag>Age: {age}</NoticesTag>
                 <NoticesTag>Price: {price}$</NoticesTag>
+                {favorite && isLogined && <NoticesTag>Favorite </NoticesTag>}
+                {isOwner && isLogined && <NoticesTag>Favorite </NoticesTag>}
               </NoticesTags>
 
               <NoticesButton>Learn More</NoticesButton>
