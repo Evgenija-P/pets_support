35a8628b15b2890348b042d78d6f1c7e785561d3
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-09 22:42:10 +0200

    clean

--- src/components/NoticesElements/NoticesCategoriesListSecond/NoticesCategoriesListSecond.jsx
+++ src/components/NoticesElements/NoticesCategoriesListSecond/NoticesCategoriesListSecond.jsx
@@ -12,7 +12,6 @@ import {
   NoticesTag,
   NoticesButton,
 } from './NoticesCategoriesListSecond.styled';
-
 import notFoundNoticesImage from '../../../img/notFoundNoticesImage.jpg';
 // import { PER_PAGE } from '../../../redux/notices/operations ';
 // import { useSelector, useDispatch } from 'react-redux';
@@ -38,21 +37,15 @@ const NoticesCategoriesListSecond = () => {
   const noticesRaw = useSelector(selectNotices);
   const favorite = useSelector(selectFavoriteList);
   const { _id: user } = useSelector(selectUser);
-
-  // const { category } = useSelector(selectNoticesObj);
   const dispatch = useDispatch();
-  // const { page: currentPage, totalHits } = useSelector(selectNoticesObj);
-
   const userFavoriteNotices = () => {
     const noticesWithFavorite = noticesRaw.map(notice => {
-      // console.log('favorite', favorite);
       if (favorite.find(fav => fav === notice._id)) {
-        // console.log('favorite', notice._id);
         return { ...notice, favorite: true };
       }
       return { ...notice, favorite: false };
     });
-    // console.log('favoriteList', noticesWithFavorite);
+
     return noticesWithFavorite;
   };
 
@@ -60,32 +53,21 @@ const NoticesCategoriesListSecond = () => {
 
   const isOwnerNotices = () => {
     const noticesOwn = noticesWithFavorite.map(notice => {
-      // console.log('user._id', user);
       if (notice.owner === user) {
-        // console.log('owner', notice.owner);
         return { ...notice, isOwner: true };
       }
       return { ...notice, isOwner: false };
     });
-    // console.log('ownerList', noticesOwn);
+
     return noticesOwn;
   };
   const notices = isOwnerNotices();
   const isLogined = useAuth();
-
-  // const OnPagination = page => {
-  //   // dispatch(setPage(page));
-  //   console.log('current page', currentPage);
-  //   dispatch(fetchNotices({ category, page }));
-  // };
-
-  // const countPages = Math.ceil(totalHits / PER_PAGE);
   const { category } = useSelector(selectNoticesObj);
   const onFavoriteToggle = (_id, favorite) => {
     if (favorite) {
       dispatch(removeFromFavorite(_id));
       if (category === '/notices/favorite') {
-        // console.log('delete/notices/favorite', _id);
         dispatch(deletefavoriteNotice(_id));
       }
 
@@ -113,11 +95,10 @@ const NoticesCategoriesListSecond = () => {
             <NoticesTop>
               <NoticesNav>
                 <NoticesBadge>{categoryName}</NoticesBadge>
-                {isLogined && (
-                  <NoticesButtonFavorite
-                    onClick={() => onFavoriteToggle(_id, favorite)}
-                  ></NoticesButtonFavorite>
-                )}
+
+                <NoticesButtonFavorite
+                  onClick={() => onFavoriteToggle(_id, favorite)}
+                ></NoticesButtonFavorite>
               </NoticesNav>
 
               <NoticesImage
