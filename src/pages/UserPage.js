8dd55a2f10dc1561e69bd83c8c80d3357f619957
Anna Riabova <annaua04@gmail.com>
2023-02-10 04:36:18 +0200

    userPets-v1

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,12 +1,17 @@
-import { Helmet } from "react-helmet";
+import { Helmet } from 'react-helmet';
+
+import PetsUserList from '../components/PetsElements/PetsList';
 
 const UserPage = () => {
-	return (
-		<div>
-			<Helmet>
-				<title>UserPage</title>
-			</Helmet>
-		</div>
-	);
+  return (
+    <div>
+      <Helmet>
+        <title>UserPage</title>
+      </Helmet>
+
+      <PetsUserList />
+    </div>
+  );
 };
+
 export default UserPage;
