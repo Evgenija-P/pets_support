a81acd6d92809ad265c3b31554da32e79c4809a5
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2023-02-11 20:22:41 +0000

    Deploying to gh-pages from @ Evgenija-P/pets_support@64b2b5c5fa3d52534d37d9a4ba838de7bec2ad2a 🚀

---
+++ static/js/856.e2514b96.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpets_support=self.webpackChunkpets_support||[]).push([[856],{6856:function(t,a,n){n.d(a,{U$P:function(){return p}});var e=n(9983);function p(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04a.996.996 0 000-1.41l-2.34-2.34a.996.996 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"}}]})(t)}}}]);
+//# sourceMappingURL=856.e2514b96.chunk.js.map
