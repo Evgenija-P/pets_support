0e47f18dafa1537f40af7083c55360e8dfc2747c
Evgenija-P <e.a.poduzova@gmail.com>
2023-02-11 15:25:46 +0200

    v3

--- src/components/Modal/ModalUnauthorized/ModalUnauthorized.jsx
+++ src/components/Modal/ModalUnauthorized/ModalUnauthorized.jsx
@@ -2,36 +2,29 @@ import React from 'react';
 import { ReactComponent as IconClose } from '../../../img/icons/close_menu.svg';
 import { CloseButton } from '../Modal.styled';
 import { ModalWrapper } from './ModalUnauthorized.styled';
-import {
-  NavLinkLog,
-  NavLinkReg,
-  ButtonLog,
-  ButtonReg,
-  Sp,
-  User,
-} from '../../AuthNav/AuthNav.styled';
+import { NavLinkR, ButtonR, Sp, UserWrapper } from './ModalUnauthorized.styled';
 
-const ModalUnauthorized = ({ children, onClose, title }) => {
+const ModalUnauthorized = ({ onClose }) => {
   return (
     <div>
-      <CloseButton type="button" onClick={onClose}>
-        <IconClose />
-      </CloseButton>{' '}
-      <User>
-        <ModalWrapper>
-          Sorry, you are not logged in. <p>Please sign up or log in</p>
-        </ModalWrapper>
-        <ButtonLog>
-          <NavLinkLog to="/login">
-            <Sp>Login</Sp>
-          </NavLinkLog>
-        </ButtonLog>
-        <ButtonReg>
-          <NavLinkReg to="/register">
-            <Sp>Registration</Sp>
-          </NavLinkReg>
-        </ButtonReg>
-      </User>
+      <ModalWrapper>
+        <CloseButton type="button" onClick={onClose}>
+          <IconClose />
+        </CloseButton>
+        Sorry, you are not logged in. <p>Please sign up or log in</p>
+        <UserWrapper>
+          <ButtonR>
+            <NavLinkR to="/login">
+              <Sp>Login</Sp>
+            </NavLinkR>
+          </ButtonR>
+          <ButtonR>
+            <NavLinkR to="/register">
+              <Sp>Registration</Sp>
+            </NavLinkR>
+          </ButtonR>
+        </UserWrapper>
+      </ModalWrapper>
     </div>
   );
 };
--- src/components/Modal/ModalUnauthorized/ModalUnauthorized.styled.js
+++ src/components/Modal/ModalUnauthorized/ModalUnauthorized.styled.js
@@ -1,11 +1,12 @@
 import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
 import { media, colors } from '../../../styles/stylesLayout';
 
 export const ModalWrapper = styled.div`
   overflow: auto;
-  width: 280px;
+  width: 320px;
   padding: 40px 20px;
-  min-height: 530px;
+  height: 230px;
   position: absolute;
   top: 50%;
   left: 50%;
@@ -14,9 +15,84 @@ export const ModalWrapper = styled.div`
   background: ${colors.white};
   z-index: 1000;
 
-  ${media.tabletAndDesktop} {
+  /* ${media.tabletAndDesktop} {
     width: 608px;
     padding: 40px 80px;
     min-height: 570px;
+  } */
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+// export const ButtonLog = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   border-radius: 40px;
+//   width: 130px;
+
+//   background: #f59256;
+//   :last-child {
+//     margin-left: 20px;
+//   }
+// `;
+
+export const ButtonR = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid #f59256;
+  border-radius: 40px;
+  width: 130px;
+
+  background: #fff;
+  :last-child {
+    margin-left: 20px;
+  }
+
+  ${media.mobile} {
+    height: 35px;
+    :last-child {
+      margin-left: 12px;
+    }
+  }
+`;
+
+// export const NavLinkLog = styled(NavLink)`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   color: #fff;
+//   padding: 10px 28px;
+
+//   text-decoration: none;
+
+//   ${media.mobile} {
+//     padding: 8px 28px;
+//   }
+// `;
+
+export const NavLinkR = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000;
+  text-decoration: none;
+  padding: 10px 28px;
+  ${media.mobile} {
+    padding: 8px 28px;
+  }
+`;
+
+export const Sp = styled.span`
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 27px;
+
+  ${media.mobile} {
+    font-size: 14px;
+    line-height: 19px;
   }
 `;
