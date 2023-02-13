ed13ee2fb91bf2ab8580f8e261b6b41a8231b7b3
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-13 14:53:34 +0200

    Update NoticesPage.js

--- src/pages/NoticesPage.js
+++ src/pages/NoticesPage.js
@@ -42,6 +42,7 @@ const NoticesPage = () => {
   //   }
   // }, [isLoggedIn]);
   // console.log('Notices', noticesList);
+  ///////////////////////////////////////////
   return (
     <>
       <Helmet>
