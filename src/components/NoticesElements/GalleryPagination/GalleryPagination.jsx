e975cd59528db01f087cda0593e105b7c9a518a3
Олег Бакалов <O.O.Bakalov@gmail.com>
2023-02-15 01:57:57 +0200

    clean

--- src/components/NoticesElements/GalleryPagination/GalleryPagination.jsx
+++ src/components/NoticesElements/GalleryPagination/GalleryPagination.jsx
@@ -4,27 +4,10 @@ import { PagginationWrapper } from './GalleryPagination.styled';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
 import { colors } from '../../../styles/stylesLayout';
 import { createTheme } from '@mui/material/styles';
-// import useAuth from '../../../hooks/useAuth';
 import { fetchNotices } from '../../../redux/notices/operations ';
 import { selectNoticesObj } from '../../../redux/notices/selectors';
 import { useDispatch, useSelector } from 'react-redux';
-// import { selectFavoriteObj } from '../../../redux/favorite/selectors';
-// import { useNavigate } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-// import { useRef } from 'react';
-
-//  accent: '#F59256',
-//   background: '#FDF7F2',
-//   accentButton: '#FF6101',
-//   black: '#111111',
-//   newsText: '#111321',
-//   maleColor: '#23C2EF',
-//   femaleColor: '#FF8787',
-//   blue: '#3091EB',
-//   white: '#FFFFFF',
-//   gray: 'rgba(17, 17, 17, 0.6)',
-//   gradient: 'linear-gradient(90deg, #FF634E 0%, #FFDF48 105.44%)',
-//   shadow: 'rgba(49, 21, 4, 0.07)',
 
 const theme = createTheme({
   palette: {
@@ -42,33 +25,17 @@ const theme = createTheme({
 });
 
 const GalleryPagination = () => {
-  const {
-    // noticesList,
-    // selectedNotice,
-    page,
-    totalHits,
-    isLoading,
-    // isAdding,
-    // error,
+  const { page, totalHits, isLoading, search, limit } =
+    useSelector(selectNoticesObj);
 
-    search,
-    limit,
-  } = useSelector(selectNoticesObj);
-  // const { isLoading: isLoadingFavorite } = useSelector(selectFavoriteObj);
-  // const { isLoggedIn } = useAuth();
   const { pathname } = useLocation();
   const dispatch = useDispatch();
-  // const firstRender = useRef(true);
 
-  // const navigate = useNavigate();
-
-  //////////////////////////////////////////////////////////////////////////////////////////////
   const OnPagination = page => {
     console.log('OnPagination', { category: pathname, page, search });
     dispatch(fetchNotices({ category: pathname, page, search }));
   };
   const countPages = Math.ceil(totalHits / limit);
-  /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   return (
     <ThemeProvider theme={theme}>
