40c100ec8be3efa15fe28166c64a32b0efa36765
Alexander Khardin <khporter1984@gmail.com>
2023-02-17 20:40:16 +0300

    add drop zone to addNoticeForm

--- src/components/Modal/ModalAddNotice/components/FileInput/FileIput.jsx
+++ src/components/Modal/ModalAddNotice/components/FileInput/FileIput.jsx
@@ -1,23 +1,36 @@
 import { VscAdd } from 'react-icons/vsc';
 import { useField } from 'formik';
+import { useDropzone } from 'react-dropzone';
+
 import * as styled from './FileInput.styled';
 
 const FileInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
+  const [field, meta, helpers] = useField(props);
 
+  const { getInputProps, getRootProps, isDragActive, isFocused } = useDropzone({
+    onDropAccepted: values => helpers.setValue(values[0], true),
+    onFileDialogCancel: () => helpers.setValue('', true),
+    multiple: false,
+  });
   const { value, ...customField } = field;
 
   return (
     <styled.Container>
       <styled.Label>{label}</styled.Label>
       <styled.UploadBtn
-        htmlFor={props.id || props.name}
         aria-label="select file button "
+        {...getRootProps({ isDragActive, isFocused })}
       >
-        <VscAdd size="50%" />
+        {isDragActive ? <styled.DnDIcon /> : <VscAdd size="50%" />}
       </styled.UploadBtn>
-      <styled.Input value="" type="file" {...customField} {...props} />
-      {meta.error && meta.touched && (
+      <styled.Input
+        value=""
+        type="file"
+        {...customField}
+        {...props}
+        {...getInputProps()}
+      />
+      {meta.error && (
         <styled.ErrorContainer>
           <styled.Error>{meta.error}</styled.Error>
         </styled.ErrorContainer>
